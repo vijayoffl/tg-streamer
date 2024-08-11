8c825b109481ea11d75533324a9d2109f1aaacfd
vijayoffl <56537792+vijayoffl@users.noreply.github.com>
2024-08-11 23:22:10 +0530

    Update files.py

--- bot/plugins/files.py
+++ bot/plugins/files.py
@@ -26,8 +26,8 @@ async def user_file_handler(event: NewMessage.Event | Message):
         logger.info(f'Generated secret code: {secret_code}')
 
         # Extract file name from document if available
-        file_name = event.document.file_name if event.document and hasattr(event.document, 'file_name') else 'unknown'
-        logger.info(f'Received file with name: {file_name}')
+        #file_name = event.document.file_name if event.document and hasattr(event.document, 'file_name') else 'unknown'
+        #logger.info(f'Received file with name: {file_name}')
         
         # Update message text
         event.message.text = f'`{secret_code}`'
@@ -48,7 +48,7 @@ async def user_file_handler(event: NewMessage.Event | Message):
     deep_link = f'https://t.me/{Telegram.BOT_USERNAME}?start=file_{message_id}_{secret_code}'
 
     if (event.document and 'video' in event.document.mime_type) or event.video:
-        stream_link = f'{Server.BASE_URL}/stream/{message_id}/{file_name}?code={secret_code}'
+        stream_link = f'{Server.BASE_URL}/stream/{message_id}/video.mkv?code={secret_code}'
         await event.reply(
             message= MediaLinksText % {'dl_link': dl_link, 'tg_link': tg_link, 'tg_link': tg_link, 'stream_link': stream_link},
             buttons=[
@@ -82,8 +82,8 @@ async def channel_file_handler(event: NewMessage.Event | Message):
     secret_code = token_hex(Telegram.SECRET_CODE_LENGTH)
 
      # Extract file name from document if available
-        file_name = event.document.file_name if event.document and hasattr(event.document, 'file_name') else 'unknown'
-        logger.info(f'Received file with name: {file_name}')
+        #file_name = event.document.file_name if event.document and hasattr(event.document, 'file_name') else 'unknown'
+        #logger.info(f'Received file with name: {file_name}')
    
     event.message.text = f"`{secret_code}`"
     message = await send_message(event.message)
@@ -93,7 +93,7 @@ async def channel_file_handler(event: NewMessage.Event | Message):
     tg_link = f"{Server.BASE_URL}/file/{message_id}?code={secret_code}"
 
     if (event.document and "video" in event.document.mime_type) or event.video:
-        stream_link = f"{Server.BASE_URL}/stream/{message_id}/{file_name}?code={secret_code}"
+        stream_link = f"{Server.BASE_URL}/stream/{message_id}/video.mkv?code={secret_code}"
 
         try:
             await event.edit(
